59f350eb704754bfc4b4af769c5e38262bd90016
Administrator <Administrator@MS-PNLHWWVDTWWZ>
2026-09-06 00:36:36 +0800

    回歸最初設定

--- L2J_Mobius_14.1_SamuraiCrow/java/org/l2jmobius/gameserver/network/serverpackets/limitshop/ExPurchaseLimitShopItemListNew.java
+++ L2J_Mobius_14.1_SamuraiCrow/java/org/l2jmobius/gameserver/network/serverpackets/limitshop/ExPurchaseLimitShopItemListNew.java
@@ -62,9 +62,6 @@ public class ExPurchaseLimitShopItemListNew extends ServerPacket
 		{
 			buffer.writeInt(product.getId());
 			buffer.writeInt(product.getProductionId());
-			// 👑 GM 強制插隊補丁：將等級限制塞在商品ID後面
-			buffer.writeInt(1);   // 最低等級要求：1級
-			buffer.writeInt(999); // 最高等級要求：999級
 			buffer.writeInt(product.getIngredientIds()[0]);
 			buffer.writeInt(product.getIngredientIds()[1]);
 			buffer.writeInt(product.getIngredientIds()[2]);
