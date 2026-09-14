51d14ead2a528a40fefdbe1b2713e7a4ad7b0c9d
Administrator <Administrator@MS-PNLHWWVDTWWZ>
2026-09-15 00:02:50 +0800

    RENEW L2J_Mobius_14.1_SamuraiCrow/java/org/l2jmobius/gameserver/ai/AttackableAI.java

--- L2J_Mobius_14.1_SamuraiCrow/java/org/l2jmobius/gameserver/ai/AttackableAI.java
+++ L2J_Mobius_14.1_SamuraiCrow/java/org/l2jmobius/gameserver/ai/AttackableAI.java
@@ -246,7 +246,7 @@ public class AttackableAI extends CreatureAI
 	public  void  notifyActionReadyToAct ()
 	{
 		// 一次成功的攻擊會讓戰鬥繼續進行，只有無法攻擊到的目標才會超時。
-		如果 ( getIntention ( )  ==  Intention.ATTACK )
+		if ( getIntention ( )  ==  Intention.ATTACK )
 		{
 			_attackTimeout  =  MAX_ATTACK_TIMEOUT  +  GameTimeTaskManager.getInstance ( ). getGameTicks ( );
 		}
@@ -258,7 +258,7 @@ public class AttackableAI extends CreatureAI
 	public  void  notifyActionFinishCasting ()
 	{
 		// 施法成功後，戰鬥將繼續進行，超時僅適用於無法到達的目標。
-		如果 ( getIntention ( )  ==  Intention.ATTACK )
+		if ( getIntention ( )  ==  Intention.ATTACK )
 		{
 			_attackTimeout  =  MAX_ATTACK_TIMEOUT  +  GameTimeTaskManager.getInstance ( ). getGameTicks ( );
 		}
